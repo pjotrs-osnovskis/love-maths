05b92cb4441443b88a86c193137f8933f58ef6ef
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-05-28 20:29:20 +0100

    set subtract functionality in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,8 +29,9 @@ function runGame(gameType) {
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
-        displayMultiplyQuestion(num1, num2);
-    
+        displayMultiplyQuestion(num1, num2); 
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else {
         // Using alert to alert user that there is an error
         alert(`Unknown game type ${gameType}`);
@@ -71,6 +72,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "*") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}, aborting!`;
@@ -99,7 +102,12 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 :
+    operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 :
+    operand1;
+    document.getElementById("operator").textContent = "-";
 
 }
 
